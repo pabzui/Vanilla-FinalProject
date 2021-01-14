ce084fa4e571f64dd692900382b14219be93ac89
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-13 17:23:27 -0700

    removed date function/ updated °c/°f function

--- src/script.js
+++ src/script.js
@@ -1,32 +1,6 @@
-let dateElement = document.querySelector("date");
-{
-  function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
+let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", convertToCelsius);
 
-    return `${day} ${hours}:${minutes}`;
-  }
-
-
-  
-  let dateElement = document.querySelector("#date");
-  let currentTime = new Date();
-  dateElement.innerHTML = formatDate(currentTime);
