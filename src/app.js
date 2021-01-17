e545ed2613cc1d41e9c4886efe83ee7a90dacfcc
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-17 12:39:24 -0700

    added units=metric to api url

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
 
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}`;
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
