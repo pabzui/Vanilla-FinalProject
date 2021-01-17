e7c265fee8e2c54802a59c4b2bf6cd9a8ae8725e
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-16 23:19:46 -0700

    update javascript

---
+++ src/app.js
@@ -0,0 +1,5 @@
+let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
+
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}`;
+
+console.log(apiUrl);
--- src/script.js
+++
@@ -1,3 +0,0 @@
-let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
-
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Fransico&appid=${apiKey}`;
