47edb46dcf6325ed390a7c20b207358bce520899
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-17 12:48:26 -0700

    add display temperature function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
+function displayTemperature(response){
+    console.log(response.data);
+}
 
+
+let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
 let apiUrl = `api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
