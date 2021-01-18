0539cef28ac66890eba86cc459b4a265793bc638
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-17 21:16:37 -0700

    added widn function.js/.html

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ function displayTemperature(response){
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = response.data.main.wind;
     
 
 }
