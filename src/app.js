9335136862342399174027b112a38a497c3b333d
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-17 20:34:46 -0700

    update city function.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 function displayTemperature(response){
     
     let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
-
+    cityElement.innerHTML = response.data.name;
 }
 
 
 let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
