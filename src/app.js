d5be1b5f8cf391e7752e9ce6ff745ac92e831ded
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-17 21:06:20 -0700

    add description of weather.js / changed style.css

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ function displayTemperature(response){
     
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+
 }
 
 
