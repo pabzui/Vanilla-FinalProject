16be1a574b0c0869655f311a27fb6c1189002bbf
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-18 12:23:40 -0700

    add/update weather icon

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -31,7 +32,9 @@ function displayTemperature(response){
     windElement.innerHTML = Math.round 
     (response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    
+    iconElement.setAttribute(
+        "src",
+     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 
