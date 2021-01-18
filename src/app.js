e1267a947224e7486d64e74581aacc3260c62864
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-18 11:51:58 -0700

    update city/ api url (changed city)

--- src/app.js
+++ src/app.js
@@ -36,6 +36,7 @@ function displayTemperature(response){
 
 
 let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Calgary&appid=${apiKey}&units=metric`;
+let city = "San Francisco"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
