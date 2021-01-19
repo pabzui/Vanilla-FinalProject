87e205e49ed9f3302d5b5fe946dd4e76c2df3590
pabzui <73762925+pabzui@users.noreply.github.com>
2021-01-18 21:47:55 -0700

    update forecast

--- src/app.js
+++ src/app.js
@@ -46,12 +46,32 @@ function searchLocation(position) {
     apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(dispalyForecast);
 }
+function displayForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    
+    forecastElement.innerHTML= `<div class="col-2">
+                <div class="card" style="width: 10rem;">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">12:00</h5>
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="">
+
+                        <p class="card-text">
+                        <div><strong>23°c </strong> || 13°c</div>
+                    </div>
+                </div>
+            </div>`;
+
+}
 
 function search(city){
     let apiKey = "2fc18d3d2a617d4f5dd286dc3cfcf411";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayTemperature);
+
+    apiUrl=`api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=metric`;
+    axios.get(apiUrl).then(displayForcast)
 }
 
 
